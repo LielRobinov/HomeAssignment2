0120bbc6161e1ed49f67e1c002fd62c709472a83
Liel Robinov <liel63312@gmail.com>
2025-05-10 02:20:44 +0300

    עדכון הפונקציה של מצב חושך , על מנת שבמעבר בין הדפים המצב ישמר עד שנלחץ בכפתור המתאים

--- services/global.js
+++ services/global.js
@@ -12,21 +12,33 @@ function toggleMenu() {
 function dark_mode(){
     const icon = document.querySelector("#icon");
     const webLogo = document.querySelector("#logo");
+    const isDark = document.body.classList.toggle("dm_colors");
 
-    document.body.classList.toggle("dm_colors");
-
-    if(document.body.classList.contains("dm_colors"))
+    if(isDark)
     {
         icon.src="assets/imgs/sun.png";
         icon.alt="sunIcon";
-        webLogo.src="assets/imgs/whiteLogo.png"
-        webLogo.alt="whiteLogo"
-
+        webLogo.src="assets/imgs/whiteLogo.png";
+        webLogo.alt="whiteLogo";
+        sessionStorage.setItem("darkMode" , "enabled");
     }
     else{
         icon.src ="assets/imgs/moon.png";
         icon.alt="WebsiteWhiteLogo";
-        webLogo.src="assets/imgs/logo3.png"
-        webLogo.alt="WebsiteLogo"
+        webLogo.src="assets/imgs/logo3.png";
+        webLogo.alt="WebsiteLogo";
+        sessionStorage.setItem("darkMode" , "disabled");
     }
+}
+
+
+if (sessionStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dm_colors");
+    const icon = document.querySelector("#icon");
+    const webLogo = document.querySelector("#logo");
+
+    icon.src="assets/imgs/sun.png";
+    icon.alt="sunIcon";
+    webLogo.src="assets/imgs/whiteLogo.png";
+    webLogo.alt="whiteLogo";
 }
