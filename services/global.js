ea0d9477a5baf1239f8e0b0fe96ae3880084cc1d
Liel Robinov <liel63312@gmail.com>
2025-05-12 13:44:42 +0300

    העברת הצבעים לקובץ של המשתנים + הורדת דברים לא רלוונטים

--- services/global.js
+++ services/global.js
@@ -13,28 +13,17 @@ function toggleMenu() {
 
 function dark_mode(){
     const icon = document.querySelector("#icon");
-    const webLogo = document.querySelector("#logo");
-    const isDark = document.body.classList.toggle("dm_colors");
+    const isDark = document.documentElement.classList.toggle("darkMode_colors");
 
     if(isDark)
     {
         icon.src="assets/imgs/sun.png";
         icon.alt="sunIcon";
-        sessionStorage.setItem("darkMode" , "enabled");
     }
     else{
         icon.src ="assets/imgs/moon.png";
         icon.alt="WebsiteWhiteLogo";
-        sessionStorage.setItem("darkMode" , "disabled");
     }
 }
 
 
-if (sessionStorage.getItem("darkMode") === "enabled") {
-    document.body.classList.add("dm_colors");
-    const icon = document.querySelector("#icon");
-    const webLogo = document.querySelector("#logo");
-
-    icon.src="assets/imgs/sun.png";
-    icon.alt="sunIcon";
-}
